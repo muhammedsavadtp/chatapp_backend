160e2580d320c5da80ba803ca155a81b0a5dc1a7
muhammed <www.savad34@gmail.com>
2025-03-16 15:11:52 +0530

    fixed group members adding issue

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,38 +1,3 @@
-// const bcrypt = require('bcryptjs');
-// const jwt = require('jsonwebtoken');
-// const User = require('../models/User');
-
-// exports.register = async (req, res) => {
-//     const { username, name, password } = req.body;
-//     try {
-//         const existingUser = await User.findOne({ username });
-//         if (existingUser) return res.status(400).json({ error: 'Username already exists' });
-//         const hashedPassword = await bcrypt.hash(password, 10);
-//         const user = new User({ username, name, password: hashedPassword });
-//         await user.save();
-//         res.status(201).json({ message: 'User registered' });
-//     } catch (error) {
-//         res.status(500).json({ error: error.message });
-//     }
-// };
-
-// exports.login = async (req, res) => {
-//     const { username, password } = req.body;
-//     try {
-//         const user = await User.findOne({ username });
-//         if (!user || !(await bcrypt.compare(password, user.password))) {
-//             return res.status(401).json({ error: 'Invalid credentials' });
-//         }
-//         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-//         res.json({
-//             token,
-//             user: { id: user._id, username, name:user.name, bio: user.bio, profilePicture: user.profilePicture },
-//         });
-//     } catch (error) {
-//         res.status(500).json({ error: error.message });
-//     }
-// };
-
 
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
--- src/routes/groupRoutes.js
+++ src/routes/groupRoutes.js
@@ -1,12 +1,26 @@
 const express = require('express');
-const { createGroup, getGroupMessages, getUserCreatedGroups, getUserJoinedGroups, addGroupMembers } = require('../controllers/groupController');
+const {
+    createGroup,
+    getGroupMessages,
+    getUserCreatedGroups,
+    getUserJoinedGroups,
+    addGroupMembers,
+    updateGroupName,
+    deleteGroup,
+    removeGroupMember,
+    addGroupAdmin,
+} = require('../controllers/groupController');
 const auth = require('../middleware/auth');
 const router = express.Router();
 
 router.post('/', auth, createGroup);
 router.get('/messages/:groupId', auth, getGroupMessages);
-router.get('/created', auth, getUserCreatedGroups); 
-router.get('/joined', auth, getUserJoinedGroups);  
+router.get('/created', auth, getUserCreatedGroups);
+router.get('/joined', auth, getUserJoinedGroups);
 router.post('/:groupId/members', auth, addGroupMembers);
+router.put('/:groupId/name', auth, updateGroupName); // New: Edit group name
+router.delete('/:groupId', auth, deleteGroup); // New: Delete group
+router.delete('/:groupId/members/:memberId', auth, removeGroupMember); // New: Remove member
+router.post('/:groupId/admins', auth, addGroupAdmin); // New: Add admin
 
 module.exports = router;
